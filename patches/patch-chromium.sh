8c41ebcafaa1f25821a05d7f5919054c798b945f
Tiago Vignatti <tiago.vignatti@intel.com>
2013-07-12 17:54:13 -0300

    Add Mesa (temporary) patch
    Signed-off-by: Tiago Vignatti <tiago.vignatti@intel.com>

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-PATCH_DIR=ozone/patches/
+
+PATCH_DIR=`pwd`/src/ozone/patches/
 
 echo "Ozone-Wayland: patching Chromium"
-touch patching_chromium_bla
-
 cd src/
 
 # making sure these patches were not applied already. if so, purge them
 git reset --hard origin/master
 
 git am $PATCH_DIR/000*
+
+# jump now to mesa dir and apply the needed patches there
+cd third_party/mesa/src
+git am $PATCH_DIR/100*
