9d30e65aa43a3d2ce22ba6bbb0ccc27c5b19d265
Tiago Vignatti <tiago.vignatti@intel.com>
2013-07-17 16:13:00 -0300

    Make the patch script a bit more robust

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-
 PATCH_DIR=`pwd`/src/ozone/patches/
+HACKING_BRANCH=master-ozone
 
 echo "Ozone-Wayland: patching Chromium"
 cd src/
 
-# making sure these patches were not applied already. if so, purge them
-git reset --hard origin/master
+# we switch to $HACKING_BRANCH before anything
+git checkout master
 
+exists=`git show-ref refs/heads/$HACKING_BRANCH`
+if [ -n "$exists" ]; then
+  git branch -D $HACKING_BRANCH
+fi
+
+git checkout -b $HACKING_BRANCH origin/master
 git am $PATCH_DIR/000*
 
 # jump now to mesa dir and apply the needed patches there
