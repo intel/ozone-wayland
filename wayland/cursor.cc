db364aa9bc754cfc8d9aa18bc2ff1f46dc0452a9
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-08-30 21:34:12 +0300

    Fix Authors List.
    Take into account review comments:
    Removed (c) after CopyRight.

--- wayland/cursor.cc
+++ wayland/cursor.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2013 Intel Corporation. All rights reserved.
+// Copyright 2013 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
