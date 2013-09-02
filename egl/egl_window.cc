1a0ddbb8927c884e6a30730e34d9e62de13c32c6
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-09-03 02:30:35 +0300

    Add EGLWindow class.
    Window currently has many responsibilites.
    One of it being to manage wl egl window.
    This patch introduces egl_window class
    which encapsulates an wl egl window and
    contains logic to create surface, resize etc.
    This code would be re-used in other places
    too. Class is only added with this patch and
    it will be taken into use in follow up patch.

---
+++ egl/egl_window.cc
@@ -0,0 +1,36 @@
+// Copyright 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ozone/egl/egl_window.h"
+
+#include "ozone/wayland/surface.h"
+#include "ozone/wayland/dispatcher.h"
+
+#include <wayland-egl.h>
+
+namespace ui {
+
+EGLWindow::EGLWindow(struct wl_surface* surface, int32_t width, int32_t height)
+    : window_(NULL)
+{
+  allocation_ = gfx::Rect(0, 0, width, height);
+  window_ = wl_egl_window_create(surface, allocation_.width(), allocation_.height());
+}
+
+EGLWindow::~EGLWindow() {
+  if (window_)
+    wl_egl_window_destroy(window_);
+}
+
+void EGLWindow::Resize(int32_t width, int32_t height)
+{
+  allocation_ = gfx::Rect(0, 0, width, height);
+  if (!window_)
+    return;
+
+  wl_egl_window_resize(window_, allocation_.x(), allocation_.y(), allocation_.width(), allocation_.height());
+  // (kalyan) handle window resize.
+}
+
+}  // namespace ui
