048e40539f639819e13aea72c64b6a49e6db0f8d
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-09-04 22:39:33 +0300

    Make input device use WaylandKeyboard and WaylandPointer classes.
    Currently, input_device is responsible for handling
    pointer and keyboard related activity. Also, in future
    it would need to support touch, selection logic, drag and
    drop etc. The main idea with this change is to be able
    to extend input_device to support all these features.
    
    The patch make changes so that input_device uses
    WaylandPointer and WaylandKeyboard classes internally
    to replace existing functionality. Any future support
    in these areas can be extended in those classes respectively.
    input_device should be responsible interface for accessing
    any type of input device.

--- wayland/input_device.cc
+++ wayland/input_device.cc
@@ -1,66 +1,18 @@
 // Copyright 2013 The Chromium Authors. All rights reserved.
-// Copyright 2013 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "ozone/wayland/input_device.h"
 
-#include <sys/mman.h>
-#include <linux/input.h>
-#include <wayland-client.h>
-
-#include "base/bind.h"
-#include "base/message_loop/message_loop.h"
-#include "base/message_loop/message_pump_ozone.h"
-#include "ozone/wayland/cursor.h"
-#include "ozone/wayland/kbd_conversion.h"
-#include "ozone/wayland/window.h"
-#include "ui/base/events/event.h"
-#include "ui/base/hit_test.h"
+#include "ozone/wayland/input_method_event_filter.h"
+#include "ozone/wayland/keyboard.h"
+#include "ozone/wayland/pointer.h"
 
 namespace ui {
 
-static WaylandCursor::CursorType cursorType(WindowLocation location)
-{
-    switch (location) {
-      case WINDOW_RESIZING_TOP:
-        return WaylandCursor::CURSOR_TOP;
-        break;
-      case WINDOW_RESIZING_BOTTOM:
-        return WaylandCursor::CURSOR_BOTTOM;
-        break;
-      case WINDOW_RESIZING_LEFT:
-        return WaylandCursor::CURSOR_LEFT;
-        break;
-      case WINDOW_RESIZING_RIGHT:
-        return WaylandCursor::CURSOR_RIGHT;
-        break;
-      case WINDOW_RESIZING_TOP_LEFT:
-        return WaylandCursor::CURSOR_TOP_LEFT;
-        break;
-      case WINDOW_RESIZING_TOP_RIGHT:
-        return WaylandCursor::CURSOR_TOP_RIGHT;
-        break;
-      case WINDOW_RESIZING_BOTTOM_LEFT:
-        return WaylandCursor::CURSOR_BOTTOM_LEFT;
-        break;
-      case WINDOW_RESIZING_BOTTOM_RIGHT:
-        return WaylandCursor::CURSOR_BOTTOM_RIGHT;
-        break;
-      case WINDOW_EXTERIOR:
-      case WINDOW_TITLEBAR:
-        default:
-        return WaylandCursor::CURSOR_LEFT_PTR;
-      }
-}
-
 WaylandInputDevice::WaylandInputDevice(WaylandDisplay* display, uint32_t id)
-  : input_seat_(NULL),
-    input_keyboard_(NULL),
-    pointer_focus_(NULL),
-    cursor_(NULL),
-    keyboard_focus_(NULL),
-    keyboard_modifiers_(0)
+  :input_keyboard_(NULL),
+   input_pointer_(NULL)
 {
   static const struct wl_seat_listener kInputSeatListener = {
     WaylandInputDevice::OnSeatCapabilities,
@@ -70,362 +22,42 @@ WaylandInputDevice::WaylandInputDevice(WaylandDisplay* display, uint32_t id)
       wl_registry_bind(display->registry(), id, &wl_seat_interface, 1));
   wl_seat_add_listener(input_seat_, &kInputSeatListener, this);
   wl_seat_set_user_data(input_seat_, this);
-
-  InitXKB();
 }
 
 WaylandInputDevice::~WaylandInputDevice()
 {
-  if (cursor_)
-    delete cursor_;
+  if (input_keyboard_)
+    delete input_keyboard_;
+
+  if (input_pointer_)
+    delete input_pointer_;
 
   if (input_seat_)
     wl_seat_destroy(input_seat_);
-
-  FiniXKB();
-}
-
-void WaylandInputDevice::InitXKB()
-{
-  xkb_.context = xkb_context_new((xkb_context_flags)0);
-  if (!xkb_.context) {
-    return;
-  }
-}
-
-void WaylandInputDevice::FiniXKB()
-{
-  if (xkb_.state)
-    xkb_state_unref(xkb_.state);
-  if (xkb_.keymap)
-    xkb_map_unref(xkb_.keymap);
-  if (xkb_.context)
-    xkb_context_unref(xkb_.context);
 }
 
 void WaylandInputDevice::OnSeatCapabilities(void *data, wl_seat *seat, uint32_t caps)
 {
   WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-
-  static const struct wl_pointer_listener kInputPointerListener = {
-    WaylandInputDevice::OnPointerEnter,
-    WaylandInputDevice::OnPointerLeave,
-    WaylandInputDevice::OnMotionNotify,
-    WaylandInputDevice::OnButtonNotify,
-    WaylandInputDevice::OnAxisNotify,
-  };
-
-  if (!device->cursor_)
-    device->cursor_ = new WaylandCursor(WaylandDisplay::GetDisplay()->shm());
-
-  if ((caps & WL_SEAT_CAPABILITY_POINTER) && !device->cursor_->GetInputPointer()) {
-    wl_pointer* input_pointer = wl_seat_get_pointer(seat);
-    device->cursor_->SetInputPointer(input_pointer);
-    wl_pointer_set_user_data(input_pointer, device);
-    wl_pointer_add_listener(input_pointer, &kInputPointerListener, device);
-  } else if (!(caps & WL_SEAT_CAPABILITY_POINTER) && device->cursor_->GetInputPointer()) {
-    wl_pointer_destroy(device->cursor_->GetInputPointer());
-    device->cursor_->SetInputPointer(NULL);
-  }
-
-  static const struct wl_keyboard_listener kInputKeyboardListener = {
-    WaylandInputDevice::OnKeyboardKeymap,
-    WaylandInputDevice::OnKeyboardEnter,
-    WaylandInputDevice::OnKeyboardLeave,
-    WaylandInputDevice::OnKeyNotify,
-    WaylandInputDevice::OnKeyModifiers,
-  };
-
   if ((caps & WL_SEAT_CAPABILITY_KEYBOARD) && !device->input_keyboard_) {
-    device->input_keyboard_ = wl_seat_get_keyboard(seat);
-    wl_keyboard_set_user_data(device->input_keyboard_, device);
-    wl_keyboard_add_listener(device->input_keyboard_, &kInputKeyboardListener,
-        device);
+    device->input_keyboard_ = new WaylandKeyboard();
   } else if (!(caps & WL_SEAT_CAPABILITY_KEYBOARD) && device->input_keyboard_) {
-    wl_keyboard_destroy(device->input_keyboard_);
+    delete device->input_keyboard_;
     device->input_keyboard_ = NULL;
   }
-}
 
-void WaylandInputDevice::DispatchEventHelper(scoped_ptr<ui::Event> key) {
-  base::MessagePumpOzone::Current()->Dispatch(key.get());
-}
-
-void WaylandInputDevice::DispatchEvent(scoped_ptr<ui::Event> event) {
-  base::MessageLoop::current()->PostTask(
-      FROM_HERE, base::Bind(&DispatchEventHelper, base::Passed(&event)));
-}
-
-void WaylandInputDevice::OnMotionNotify(void* data,
-    wl_pointer* input_pointer,
-    uint32_t time,
-    wl_fixed_t sx_w,
-    wl_fixed_t sy_w)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  float sx = wl_fixed_to_double(sx_w);
-  float sy = wl_fixed_to_double(sy_w);
-
-  device->pointer_position_.SetPoint(sx, sy);
-
-  scoped_ptr<MouseEvent> mouseev(new MouseEvent(
-      ui::ET_MOUSE_MOVED,
-      gfx::Point(sx, sy),
-      gfx::Point(sx, sy),
-      /* flags */ 0));
-
-  DispatchEvent(mouseev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnAxisNotify(void* data,
-    wl_pointer* input_pointer,
-    uint32_t time,
-    uint32_t axis,
-    int32_t value)
-{
-  int x_offset = 0, y_offset = 0;
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  const int delta = ui::MouseWheelEvent::kWheelDelta;
-
-  switch (axis) {
-  case WL_POINTER_AXIS_HORIZONTAL_SCROLL:
-    x_offset = value > 0 ? -delta : delta;
-    break;
-  case WL_POINTER_AXIS_VERTICAL_SCROLL:
-    y_offset = value > 0 ? -delta : delta;
-    break;
+  if ((caps & WL_SEAT_CAPABILITY_POINTER) && !device->input_pointer_) {
+    device->input_pointer_ = new WaylandPointer();
+  } else if (!(caps & WL_SEAT_CAPABILITY_POINTER) && device->input_pointer_) {
+    delete device->input_pointer_;
+    device->input_pointer_ = NULL;
   }
 
-  MouseEvent mouseev(
-      ui::ET_MOUSEWHEEL,
-      device->pointer_position_,
-      device->pointer_position_,
-      /* flags */ 0);
+  if (device->input_keyboard_)
+    device->input_keyboard_->OnSeatCapabilities(seat, caps);
 
-  scoped_ptr<MouseWheelEvent> wheelev(new MouseWheelEvent(
-      mouseev,
-      x_offset,
-      y_offset));
-
-  DispatchEvent(wheelev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnButtonNotify(void* data,
-    wl_pointer* input_pointer,
-    uint32_t serial,
-    uint32_t time,
-    uint32_t button,
-    uint32_t state)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
-
-  EventType type;
-  if (state == WL_POINTER_BUTTON_STATE_PRESSED)
-    type = ui::ET_MOUSE_PRESSED;
-  else
-    type = ui::ET_MOUSE_RELEASED;
-
-  // TODO(vignatti): simultaneous clicks fail
-  int flags = 0;
-  if (button == BTN_LEFT)
-    flags = ui::EF_LEFT_MOUSE_BUTTON;
-  else if (button == BTN_RIGHT)
-    flags = ui::EF_RIGHT_MOUSE_BUTTON;
-  else if (button == BTN_MIDDLE)
-    flags = ui::EF_MIDDLE_MOUSE_BUTTON;
-
-  scoped_ptr<MouseEvent> mouseev(new MouseEvent(
-      type,
-      device->pointer_position_,
-      device->pointer_position_,
-      flags));
-
-  DispatchEvent(mouseev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnKeyNotify(void* data,
-    wl_keyboard* input_keyboard,
-    uint32_t serial,
-    uint32_t time,
-    uint32_t key,
-    uint32_t state)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  uint32_t code, num_syms;
-  const xkb_keysym_t *syms;
-  xkb_keysym_t sym;
-  xkb_mod_mask_t mask;
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
-
-  EventType type;
-  if (state == WL_KEYBOARD_KEY_STATE_PRESSED)
-    type = ET_KEY_PRESSED;
-  else
-    type = ET_KEY_RELEASED;
-
-  code = key + 8;
-  num_syms = xkb_key_get_syms(device->xkb_.state, code, &syms);
-  if(num_syms == 1)
-    sym = syms[0];
-  else
-    sym = XKB_KEY_NoSymbol;
-
-  mask = xkb_state_serialize_mods(device->xkb_.state,
-      (xkb_state_component)(XKB_STATE_DEPRESSED | XKB_STATE_LATCHED));
-  device->keyboard_modifiers_ = 0;
-  if (mask & device->xkb_.control_mask)
-    device->keyboard_modifiers_ |= EF_CONTROL_DOWN;
-  if (mask & device->xkb_.alt_mask)
-    device->keyboard_modifiers_ |= EF_ALT_DOWN;
-  if (mask & device->xkb_.shift_mask)
-    device->keyboard_modifiers_ |= EF_SHIFT_DOWN;
-
-  scoped_ptr<KeyEvent> keyev(new KeyEvent(
-      type,
-      ui::KeyboardCodeFromXKeysym(sym),
-      device->keyboard_modifiers_,
-      true));
-
-  DispatchEvent(keyev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnKeyModifiers(void *data, wl_keyboard *keyboard,
-    uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched,
-    uint32_t mods_locked, uint32_t group)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-
-  xkb_state_update_mask(device->xkb_.state, mods_depressed, mods_latched,
-      mods_locked, 0, 0, group);
-}
-
-void WaylandInputDevice::OnPointerEnter(void* data,
-    wl_pointer* input_pointer,
-    uint32_t serial,
-    wl_surface* surface,
-    wl_fixed_t sx_w,
-    wl_fixed_t sy_w)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  WaylandWindow* window;
-  float sx = wl_fixed_to_double(sx_w);
-  float sy = wl_fixed_to_double(sy_w);
-
-  device->pointer_position_.SetPoint(sx, sy);
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
-  device->pointer_enter_serial_ = serial;
-
-  // If we have a surface, then a new window is in focus
-  window = static_cast<WaylandWindow*>(wl_surface_get_user_data(surface));
-  device->pointer_focus_ = window;
-
-  // TODO(vignatti): sx and sy have to be used for setting different resizing
-  // and other cursors.
-
-  device->cursor_->Update(cursorType(WINDOW_TITLEBAR), serial);
-  scoped_ptr<MouseEvent> mouseev(new MouseEvent(
-      ui::ET_MOUSE_ENTERED,
-      device->pointer_position_,
-      device->pointer_position_,
-      /* flags */ 0));
-
-  DispatchEvent(mouseev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnPointerLeave(void* data,
-    wl_pointer* input_pointer,
-    uint32_t serial,
-    wl_surface* surface)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  WaylandWindow* window = device->pointer_focus_;
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
-
-  device->pointer_focus_ = NULL;
-
-  scoped_ptr<MouseEvent> mouseev(new MouseEvent(
-      ui::ET_MOUSE_EXITED,
-      device->pointer_position_,
-      device->pointer_position_,
-      /* flags */ 0));
-
-  DispatchEvent(mouseev.PassAs<ui::Event>());
-}
-
-void WaylandInputDevice::OnKeyboardKeymap(void *data,
-    struct wl_keyboard *keyboard,
-    uint32_t format, int fd, uint32_t size)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  char *map_str;
-
-  if (!data) {
-    close(fd);
-    return;
-  }
-
-  if (format != WL_KEYBOARD_KEYMAP_FORMAT_XKB_V1) {
-    close(fd);
-    return;
-  }
-
-  map_str = (char*) mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
-  if (map_str == MAP_FAILED) {
-    close(fd);
-    return;
-  }
-
-  device->xkb_.keymap = xkb_map_new_from_string(device->xkb_.context,
-      map_str, XKB_KEYMAP_FORMAT_TEXT_V1, (xkb_map_compile_flags)0);
-  munmap(map_str, size);
-  close(fd);
-  if (!device->xkb_.keymap) {
-    return;
-  }
-
-  device->xkb_.state = xkb_state_new(device->xkb_.keymap);
-  if (!device->xkb_.state) {
-    xkb_map_unref(device->xkb_.keymap);
-    device->xkb_.keymap = NULL;
-    return;
-  }
-
-  device->xkb_.control_mask =
-    1 << xkb_map_mod_get_index(device->xkb_.keymap, "Control");
-  device->xkb_.alt_mask =
-    1 << xkb_map_mod_get_index(device->xkb_.keymap, "Mod1");
-  device->xkb_.shift_mask =
-    1 << xkb_map_mod_get_index(device->xkb_.keymap, "Shift");
-}
-
-void WaylandInputDevice::OnKeyboardEnter(void* data,
-    wl_keyboard* input_keyboard,
-    uint32_t serial,
-    wl_surface* surface,
-    wl_array* keys)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  WaylandWindow* window;
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
-  window = device->keyboard_focus_ =
-    static_cast<WaylandWindow*>(wl_surface_get_user_data(surface));
-}
-
-void WaylandInputDevice::OnKeyboardLeave(void* data,
-    wl_keyboard* input_keyboard,
-    uint32_t serial,
-    wl_surface* surface)
-{
-  WaylandInputDevice* device = static_cast<WaylandInputDevice*>(data);
-  WaylandWindow* window = device->keyboard_focus_;
-
-  WaylandDisplay::GetDisplay()->SetSerial(serial);
+  if (device->input_pointer_)
+    device->input_pointer_->OnSeatCapabilities(seat, caps);
 }
 
 }  // namespace ui
--- wayland/input_device.h
+++ wayland/input_device.h
@@ -1,171 +1,38 @@
 // Copyright 2013 The Chromium Authors. All rights reserved.
-// Copyright 2013 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef OZONE_WAYLAND_INPUT_DEVICE_H_
 #define OZONE_WAYLAND_INPUT_DEVICE_H_
 
-#include <stdint.h>
-
-#include <xkbcommon/xkbcommon.h>
-#include "base/basictypes.h"
-#include "base/memory/scoped_ptr.h"
 #include "ozone/wayland/display.h"
-#include "ozone/wayland/global.h"
-#include "ui/gfx/point.h"
 
 namespace ui {
 
 class Event;
-class WaylandWindow;
-class WaylandCursor;
+class WaylandKeyboard;
+class WaylandPointer;
+class InputMethod;
+class WaylandInputMethodEventFilter;
 
-// This class represents an input device that was registered with Wayland.
-// The purpose of this class is to parse and wrap events into generic
-// ui.Event types and dispatch the event to the appropriate window that has
-// assigned to the dispatcher.
-//
-// How Wayland events work:
-// ------------------------
-//
-// When the On*Focus events are triggered, the input device receives a
-// reference to the surface that just received/lost focus. Each surface is
-// associated with a unique WaylandWindow. When processing the focus events we
-// keep track of the currently focused window such that when we receive
-// different events (mouse button press or key press) we only send the event to
-// the window in focus.
 class WaylandInputDevice {
  public:
   WaylandInputDevice(WaylandDisplay* display, uint32_t id);
   ~WaylandInputDevice();
 
   wl_seat* GetInputSeat() { return input_seat_; }
-  WaylandCursor* cursor() { return cursor_; }
+  WaylandKeyboard* GetKeyBoard() const { return input_keyboard_; }
+  WaylandPointer* GetPointer() const { return input_pointer_; }
 
  private:
-  static void DispatchEventHelper(scoped_ptr<ui::Event> key);
-  static void DispatchEvent(scoped_ptr<ui::Event> event);
-
-  static void OnMotionNotify(
-      void* data,
-      wl_pointer* input_pointer,
-      uint32_t time,
-      wl_fixed_t sx_w,
-      wl_fixed_t sy_w);
-
-  static void OnButtonNotify(
-      void* data,
-      wl_pointer* input_pointer,
-      uint32_t serial,
-      uint32_t time,
-      uint32_t button,
-      uint32_t state);
-
-  static void OnKeyNotify(
-      void* data,
-      wl_keyboard* input_keyboard,
-      uint32_t serial,
-      uint32_t time,
-      uint32_t key,
-      uint32_t state);
-
-  static void OnAxisNotify(
-      void* data,
-      wl_pointer* input_pointer,
-      uint32_t time,
-      uint32_t axis,
-      int32_t value);
-
-  // On*Focus events also have a Wayland surface associated with them. If the
-  // surface is NULL, then the event signifies a loss of focus. Otherwise we
-  // use the surface to get the WaylandWindow that receives focus.
-  static void OnPointerEnter(
-      void* data,
-      wl_pointer* input_pointer,
-      uint32_t serial,
-      wl_surface* surface,
-      wl_fixed_t sx_w,
-      wl_fixed_t sy_w);
-
-  static void OnPointerLeave(
-      void* data,
-      wl_pointer* input_pointer,
-      uint32_t serial,
-      wl_surface* surface);
-
-  static void OnKeyboardKeymap(
-      void *data,
-      struct wl_keyboard *keyboard,
-      uint32_t format,
-      int fd,
-      uint32_t size);
-
-  static void OnKeyboardEnter(
-      void* data,
-      wl_keyboard* input_keyboard,
-      uint32_t serial,
-      wl_surface* surface,
-      wl_array* keys);
-
-  static void OnKeyboardLeave(
-      void* data,
-      wl_keyboard* input_keyboard,
-      uint32_t serial,
-      wl_surface* surface);
-
-  static void OnKeyModifiers(
-      void *data,
-      wl_keyboard *keyboard,
-      uint32_t serial,
-      uint32_t mods_depressed,
-      uint32_t mods_latched,
-      uint32_t mods_locked,
-      uint32_t group);
-
   static void OnSeatCapabilities(
       void *data,
       wl_seat *seat,
       uint32_t caps);
 
   wl_seat* input_seat_;
-  wl_keyboard* input_keyboard_;
-
-  WaylandCursor* cursor_;
-
-  // These keep track of the window that's currently under focus. NULL if no
-  // window is under focus.
-  WaylandWindow* pointer_focus_;
-  WaylandWindow* keyboard_focus_;
-
-  // Keeps track of the currently active keyboard modifiers. We keep this
-  // since we want to advertise keyboard modifiers with mouse events.
-  uint32_t keyboard_modifiers_;
-
-  // Keeps track of the last position for the motion event. We want to
-  // dispatch this with events such as wheel or button which don't have a
-  // position associated on Wayland.
-  gfx::Point pointer_position_;
-
-  // Keep track of the time of last event. Useful when we get buffer Attach
-  // calls and the calls wouldn't have a way of getting an event time.
-  uint32_t pointer_enter_serial_;
-
-  // keymap used to transform keyboard events.
-  struct xkb {
-    struct xkb_rule_names names;
-    struct xkb_keymap *keymap;
-    struct xkb_state *state;
-    struct xkb_context *context;
-    xkb_mod_mask_t control_mask;
-    xkb_mod_mask_t alt_mask;
-    xkb_mod_mask_t shift_mask;
-  };
-
-  xkb xkb_;
-
-  void InitXKB();
-  void FiniXKB();
+  WaylandKeyboard* input_keyboard_;
+  WaylandPointer* input_pointer_;
 
   DISALLOW_COPY_AND_ASSIGN(WaylandInputDevice);
 };
