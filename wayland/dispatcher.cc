d4cbbdd975af8d7970b446a6d2fd679db6ea65c9
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-09-11 20:19:57 +0300

    Add polling support in Dispatcher.
    This patch adds support for polling on wayland display fd in a IO thread when
    in multiprocess mode. The code is adopted from window class in weston clients.
    WaylandOzoneDisplay would decide when to use this or EventFactoryWayland as
    needed.

--- wayland/dispatcher.cc
+++ wayland/dispatcher.cc
@@ -3,16 +3,92 @@
 // found in the LICENSE file.
 
 #include "ozone/wayland/dispatcher.h"
+
+#include "ozone/wayland/kbd_conversion.h"
+
 #include "base/bind.h"
 #include "base/message_loop/message_pump_ozone.h"
-#include <stdio.h>
+#include "content/child/child_thread.h"
+#include "content/child/child_process.h"
+#include "ui/base/events/event.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 namespace ui {
+WaylandDispatcher* WaylandDispatcher::instance_ = NULL;
 
-WaylandDispatcher::WaylandDispatcher()
-    : Thread("WaylandDispatcher"),
-      ignore_task_(false)
+// os-compatibility
+extern "C" {
+int osEpollCreateCloExec(void);
+
+static int setCloExecOrClose(int fd)
 {
+  long flags;
+
+  if (fd == -1)
+    return -1;
+
+  flags = fcntl(fd, F_GETFD);
+  if (flags == -1)
+    goto err;
+
+  if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+    goto err;
+
+  return fd;
+
+err:
+  close(fd);
+  return -1;
+}
+
+int osEpollCreateCloExec(void)
+{
+  int fd;
+
+#ifdef EPOLL_CLOEXEC
+  fd = epoll_create1(EPOLL_CLOEXEC);
+  if (fd >= 0)
+    return fd;
+  if (errno != EINVAL)
+    return -1;
+#endif
+
+  fd = epoll_create(1);
+  return setCloExecOrClose(fd);
+}
+
+} // os-compatibility
+
+
+content::ChildThread* GetProcessMainThread()
+{
+  content::ChildProcess* process = content::ChildProcess::current();
+  DCHECK(process);
+  DCHECK(process->main_thread());
+  return process ? process->main_thread() : NULL;
+}
+
+WaylandDispatcher::WaylandDispatcher(int fd)
+    : Thread("WaylandDispatcher"),
+      ignore_task_(false),
+      running(false),
+      epoll_fd_(0),
+      display_fd_(fd)
+{
+  instance_ = this;
+  if (display_fd_) {
+    epoll_fd_ = osEpollCreateCloExec();
+    struct epoll_event ep;
+    ep.events = EPOLLIN | EPOLLOUT;
+    ep.data.ptr = 0;
+    epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, display_fd_, &ep);
+  }
+
   loop_ = base::MessageLoop::current();
   Options options;
   options.message_loop_type = base::MessageLoop::TYPE_IO;
@@ -20,8 +96,19 @@ WaylandDispatcher::WaylandDispatcher()
   SetPriority(base::kThreadPriority_Background);
 }
 
-void WaylandDispatcher::DispatchEventHelper(scoped_ptr<ui::Event> key) {
-  base::MessagePumpOzone::Current()->Dispatch(key.get());
+WaylandDispatcher::~WaylandDispatcher()
+{
+  ignore_task_ = true;
+  loop_ = NULL;
+  running = false;
+  Stop();
+
+  if (epoll_fd_) {
+    close(epoll_fd_);
+    epoll_fd_ = 0;
+  }
+
+  instance_ = NULL;
 }
 
 void WaylandDispatcher::PostTask(Task type)
@@ -29,13 +116,20 @@ void WaylandDispatcher::PostTask(Task type)
   if (!IsRunning() || ignore_task_)
     return;
 
-  switch(type) {
+  switch (type) {
     case (Flush):
       message_loop_proxy()->PostTask(
                   FROM_HERE, base::Bind(&WaylandDispatcher::HandleFlush));
       break;
-    default:
-      break;
+    case (Poll):
+      if (epoll_fd_) {
+        loop_ = base::MessageLoop::current();
+        if (!running)
+          message_loop_proxy()->PostTask(FROM_HERE, base::Bind(
+                                          &WaylandDispatcher::DisplayRun, this));
+      }
+  default:
+    break;
   }
 }
 
@@ -46,6 +140,7 @@ void WaylandDispatcher::MessageLoopDestroyed()
 
   ignore_task_ = true;
   loop_ = NULL;
+  running = false;
   Stop();
 }
 
@@ -59,26 +154,17 @@ void WaylandDispatcher::PostTaskOnMainLoop(
 }
 
 void WaylandDispatcher::DispatchEvent(scoped_ptr<ui::Event> event) {
-  PostTaskOnMainLoop(FROM_HERE, base::Bind(&DispatchEventHelper,
+  PostTaskOnMainLoop(FROM_HERE, base::Bind(&WaylandDispatcher::DispatchEventHelper,
                                            base::Passed(&event)));
 }
 
-WaylandDispatcher::~WaylandDispatcher()
-{
-  ignore_task_ = true;
-  loop_ = NULL;
-  Stop();
+void WaylandDispatcher::DispatchEventHelper(scoped_ptr<ui::Event> key) {
+  base::MessagePumpOzone::Current()->Dispatch(key.get());
 }
 
 void WaylandDispatcher::HandleFlush()
 {
-  WaylandDisplay* display = WaylandDisplay::GetDisplay();
-  if (!display)
-    return;
-
-  wl_display* waylandDisp = display->display();
-  if (!waylandDisp)
-    return;
+  wl_display* waylandDisp = WaylandDisplay::GetInstance()->display();
 
   while (wl_display_prepare_read(waylandDisp) != 0)
     wl_display_dispatch_pending(waylandDisp);
@@ -86,6 +172,57 @@ void WaylandDispatcher::HandleFlush()
   wl_display_flush(waylandDisp);
   wl_display_read_events(waylandDisp);
   wl_display_dispatch_pending(waylandDisp);
+}
+
+void  WaylandDispatcher::DisplayRun(WaylandDispatcher* data)
+{
+  struct epoll_event ep[16];
+  int i, count, ret;
+
+  data->running = 1;
+  // Adopted from:
+  // http://cgit.freedesktop.org/wayland/weston/tree/clients/window.c#n5531.
+  while (1) {
+    wl_display* waylandDisp = WaylandDisplay::GetInstance()->display();
+    wl_display_dispatch_pending(waylandDisp);
+
+    if (!data->running)
+      break;
+
+    ret = wl_display_flush(waylandDisp);
+    if (ret < 0 && errno == EAGAIN) {
+      ep[0].events = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLHUP;
+      epoll_ctl(data->epoll_fd_, EPOLL_CTL_MOD, data->display_fd_, &ep[0]);
+    } else if (ret < 0) {
+      break;
+    }
+
+    count = epoll_wait(data->epoll_fd_, ep, 16, -1);
+    for (i = 0; i < count; i++) {
+      struct epoll_event eps;
+      int ret;
+      uint32_t event = ep[i].events;
+
+      if (event & EPOLLERR || event & EPOLLHUP)
+        return;
+
+      if (event & EPOLLIN) {
+        ret = wl_display_dispatch(waylandDisp);
+        if (ret == -1)
+          return;
+      }
+
+      if (event & EPOLLOUT) {
+        ret = wl_display_flush(waylandDisp);
+        if (ret == 0) {
+          eps.events = EPOLLIN | EPOLLERR | EPOLLHUP;
+          epoll_ctl(data->epoll_fd_, EPOLL_CTL_MOD, data->display_fd_, &eps);
+        } else if (ret == -1 && errno != EAGAIN) {
+          return;
+        }
+      }
+    }
+  }
 }
 
 }  // namespace ui
--- wayland/dispatcher.h
+++ wayland/dispatcher.h
@@ -10,25 +10,39 @@
 #include "ui/base/events/event.h"
 #include "ozone/wayland/display.h"
 
+namespace OzoneWayland {
+class OzoneDisplay;
+class OzoneDisplayChannel;
+}
+
 namespace ui {
 class EventFactoryWayland;
-class WaylandDisplay;
 
-// WaylandDispatcher can be used to
-// read any pending events and flush
-// wayland display in a worker thread.
+// WaylandDispatcher class is used by OzoneDisplay for reading pending events
+// coming from Wayland compositor and flush requests back. WaylandDispatcher is
+// performed entirely in a separate IO thread and it can use polling as needed
+// for the operations.
+
+// WaylandDispatcher is also used by input classes to send related events.
+// In case of single process:
+// Dispatcher creates appropriate event and posts it to message loop of mainthread.
+// The event is than dispatched to MessagePumpOzone.
+// In case of MultiProcess:
+// Dispatcher posts task to childthread, from here it is sent via IPC channel to
+// Browser.
 
 class WaylandDispatcher : public base::Thread {
-public:
+ public:
   enum Task
   {
-    Flush = 0x01 // Handles Flush in worker thread.
+    Flush = 0x01, // Handles Flush in worker thread.
+    Poll = 0x02  // To poll on a display fd. Task has no effect in case
+                 // a valid display fd is not passed to WaylandDispatcher.
   };
 
-  WaylandDispatcher();
-  virtual ~WaylandDispatcher();
+  static WaylandDispatcher* GetInstance() { return instance_; }
 
-  // Posts task to worker thread, read pending events and flush display.
+  // Posts task to worker thread.
   void PostTask(Task type = Flush);
   // Dispatches event to MessagePumpOzone.
   void DispatchEvent(scoped_ptr<ui::Event> event);
@@ -36,13 +50,22 @@ public:
   void PostTaskOnMainLoop(const tracked_objects::Location& from_here,
                           const base::Closure& task);
 
-private:
+ private:
+  WaylandDispatcher(int fd = 0);
+  virtual ~WaylandDispatcher();
   static void HandleFlush();
+  static void DisplayRun(WaylandDispatcher* data);
   static void DispatchEventHelper(scoped_ptr<ui::Event> key);
   void MessageLoopDestroyed();
   base::MessageLoop* loop_;
   bool ignore_task_ :1;
+  bool running :1;
+  int epoll_fd_;
+  int display_fd_;
+  static WaylandDispatcher* instance_;
   friend class EventFactoryWayland;
+  friend class OzoneWayland::OzoneDisplay;
+  friend class OzoneWayland::OzoneDisplayChannel;
   DISALLOW_COPY_AND_ASSIGN(WaylandDispatcher);
 };
 
