1374fb7195f950899726710645f797539f9eae03
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-09-11 18:45:43 +0300

    Add OzoneDisplayChannelHost and OzoneDisplayChannel.
    OzoneDisplayChannel is responsible for listening to any messages sent by it's
    host counterpart in BrowserProcess. There will be only one OzoneDisplay Channel
    per browser instance.
    
    OzoneDisplayChannelHost is responsible for listening to any relevant messages
    sent from gpu process(i.e dispatcher and OzoneDisplayChannel). There will be
    always only one OzoneDisplayChannelHost per browser instance. It listens to
    these messages in IO thread. Any message which needs to be handled on UI
    thread is than posted to it via dispatcher.

---
+++ impl/ipc/display_channel.h
@@ -0,0 +1,38 @@
+// Copyright 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef OZONE_WAYLAND_DISPLAY_CHANNEL_H_
+#define OZONE_WAYLAND_DISPLAY_CHANNEL_H_
+
+#include "ozone/wayland/dispatcher.h"
+#include "ipc/ipc_listener.h"
+
+namespace OzoneWayland {
+
+// OzoneDisplayChannel is responsible for listening to any messages sent by it's
+// host counterpart in BrowserProcess. There will be always only one
+// OzoneDisplayChannel per browser instance.
+
+class OzoneDisplayChannel : public IPC::Listener
+{
+ public:
+  OzoneDisplayChannel(unsigned display_fd);
+  ~OzoneDisplayChannel();
+
+  // IPC::Listener implementation.
+  virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
+
+  void OnEstablishChannel(unsigned route_id);
+  void Register();
+
+ private:
+  unsigned display_fd_;
+  unsigned route_id_;
+  bool mapped_ :1;
+  DISALLOW_COPY_AND_ASSIGN(OzoneDisplayChannel);
+};
+
+}  // namespace ui
+
+#endif  // OZONE_WAYLAND_DISPLAY_CHANNEL_H_
