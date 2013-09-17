c127b04169bdc97baf63a7e23582bdd5f61eb991
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-09-17 21:04:38 +0300

    Move inputs file to a separate folder.
    This patch moves input related files to under inputs folder.

--- wayland/inputs/cursor.cc
+++ wayland/inputs/cursor.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ozone/wayland/cursor.h"
+#include "ozone/wayland/inputs/cursor.h"
 #include "ozone/wayland/surface.h"
 
 namespace ozonewayland {
