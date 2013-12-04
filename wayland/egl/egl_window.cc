ee6b47382dcf3504fb86d04343b51f02e3cb5864
Michael Forney <mforney@mforney.org>
2013-12-04 13:31:24 -0800

    Fix argument order in wl_egl_window_resize
    The prototype is:
    
        void wl_egl_window_resize(struct wl_egl_window *egl_window,
                                  int width, int height, int dx, int dy)
    
    This fixes #151

--- wayland/egl/egl_window.cc
+++ wayland/egl/egl_window.cc
@@ -27,7 +27,7 @@ bool EGLWindow::Resize(WaylandSurface* surface, int32_t width, int32_t height)
   surface->EnsureFrameCallBackDone();
 
   // TODO(kalyan): Check if we need to sync display here.
-  wl_egl_window_resize(window_, 0, 0, width, height);
+  wl_egl_window_resize(window_, width, height, 0, 0);
   return true;
 }
 
