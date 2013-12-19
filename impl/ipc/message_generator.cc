30ae7b985f2a598075b270e6852da6f009416b36
Kondapally Kalyan <kalyan.kondapally@intel.com>
2013-12-19 13:17:33 +0200

    Ignore Lint warnings in Messages.
    Messages includes ipc messages and MessageGenerator has duplicated
    includes as needed by the IPC system. Add exceptions for both these
    files to avoid warnings from lint.

--- impl/ipc/message_generator.cc
+++ impl/ipc/message_generator.cc
@@ -8,26 +8,26 @@
 
 // Generate constructors.
 #include "ipc/struct_constructor_macros.h"
-#include "ozone/impl/ipc/message_generator.h"
+#include "ozone/impl/ipc/message_generator.h"  // NOLINT(build/include)
 
 // Generate destructors.
 #include "ipc/struct_destructor_macros.h"
-#include "ozone/impl/ipc/message_generator.h"
+#include "ozone/impl/ipc/message_generator.h"  // NOLINT(build/include)
 
 // Generate param traits write methods.
-#include "ipc/param_traits_write_macros.h"
+#include "ipc/param_traits_write_macros.h"  // NOLINT(build/include_alpha)
 namespace IPC {
-#include "ozone/impl/ipc/message_generator.h"
+#include "ozone/impl/ipc/message_generator.h"  // NOLINT(build/include)
 }  // namespace IPC
 
 // Generate param traits read methods.
-#include "ipc/param_traits_read_macros.h"
+#include "ipc/param_traits_read_macros.h"  // NOLINT(build/include_alpha)
 namespace IPC {
-#include "ozone/impl/ipc/message_generator.h"
+#include "ozone/impl/ipc/message_generator.h"  // NOLINT(build/include)
 }  // namespace IPC
 
 // Generate param traits log methods.
-#include "ipc/param_traits_log_macros.h"
+#include "ipc/param_traits_log_macros.h"  // NOLINT(build/include_alpha)
 namespace IPC {
-#include "ozone/impl/ipc/message_generator.h"
+#include "ozone/impl/ipc/message_generator.h"  // NOLINT(build/include)
 }  // namespace IPC
--- impl/ipc/messages.h
+++ impl/ipc/messages.h
@@ -7,66 +7,65 @@
 #include "base/basictypes.h"
 #include "base/strings/string16.h"
 #include "ipc/ipc_message_macros.h"
-#include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_message_utils.h"
 #include "ipc/ipc_param_traits.h"
 #include "ipc/param_traits_macros.h"
 
 #define IPC_MESSAGE_START LastIPCMsgStart
 
-IPC_MESSAGE_CONTROL2(WaylandInput_MotionNotify,
+IPC_MESSAGE_CONTROL2(WaylandInput_MotionNotify,  // NOLINT(readability/fn_size)
                      float /*x*/,
                      float /*y*/)
 
-IPC_MESSAGE_CONTROL5(WaylandInput_ButtonNotify,
+IPC_MESSAGE_CONTROL5(WaylandInput_ButtonNotify,  // NOLINT(readability/fn_size)
                      unsigned /*handle*/,
                      int /*state*/,
                      int /*flags*/,
                      float /*x*/,
                      float /*y*/)
 
-IPC_MESSAGE_CONTROL4(WaylandInput_AxisNotify,
+IPC_MESSAGE_CONTROL4(WaylandInput_AxisNotify,  // NOLINT(readability/fn_size)
                      float /*x*/,
                      float /*y*/,
                      float /*x_offset*/,
                      float /*y_offset*/)
 
-IPC_MESSAGE_CONTROL3(WaylandInput_PointerEnter,
+IPC_MESSAGE_CONTROL3(WaylandInput_PointerEnter,  // NOLINT(readability/fn_size)
                      unsigned /*handle*/,
                      float /*x*/,
                      float /*y*/)
 
-IPC_MESSAGE_CONTROL3(WaylandInput_PointerLeave,
+IPC_MESSAGE_CONTROL3(WaylandInput_PointerLeave,  // NOLINT(readability/fn_size)
                      unsigned /*handle*/,
                      float /*x*/,
                      float /*y*/)
 
-IPC_MESSAGE_CONTROL3(WaylandInput_KeyNotify,
+IPC_MESSAGE_CONTROL3(WaylandInput_KeyNotify,  // NOLINT(readability/fn_size)
                      unsigned /*type*/,
                      unsigned /*code*/,
                      unsigned /*modifiers*/)
 
-IPC_MESSAGE_CONTROL2(WaylandInput_OutputSize,
+IPC_MESSAGE_CONTROL2(WaylandInput_OutputSize,  // NOLINT(readability/fn_size)
                      unsigned /*width*/,
                      unsigned /*height*/)
 
 // Response from DisplayChannelHost to DisplayChannel as an ack to connection
 // request.
-IPC_MESSAGE_ROUTED0(WaylandMsg_DisplayChannelEstablished)
+IPC_MESSAGE_ROUTED0(WaylandMsg_DisplayChannelEstablished)  // NOLINT
 
-IPC_MESSAGE_ROUTED4(WaylandWindow_State,
+IPC_MESSAGE_ROUTED4(WaylandWindow_State,  // NOLINT(readability/fn_size)
                     unsigned /* window handle */,
                     unsigned /*state*/,
                     unsigned /*width*/,
                     unsigned /*height*/)
 
-IPC_MESSAGE_ROUTED5(WaylandWindow_Attributes,
+IPC_MESSAGE_ROUTED5(WaylandWindow_Attributes,  // NOLINT(readability/fn_size)
                     unsigned /* window handle */,
                     unsigned /* window parent */,
                     unsigned /* x */,
                     unsigned /* y */,
                     unsigned /* window type */)
 
-IPC_MESSAGE_ROUTED2(WaylandWindow_Title,
+IPC_MESSAGE_ROUTED2(WaylandWindow_Title,  // NOLINT(readability/fn_size)
                     unsigned /* window handle */,
                     string16 /* window title */)
