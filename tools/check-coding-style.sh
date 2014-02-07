d8158d20ea9921d5fcb9b3306b23c75b66ad69c0
Tiago Vignatti <tiago.vignatti@intel.com>
2014-02-07 15:27:28 +0200

    tools: Add script for coding style checking
    For now it should be enough to remember ourselves to run this script from time
    to time. I think we'll get used to this habit. Later we can think in a
    pre-commit git hook or something to enforce that all developers are running it:
    
    http://stackoverflow.com/questions/3462955/putting-git-hooks-into-repository
    
    Fixes #91

---
+++ tools/check-coding-style.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) 2014 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# stolen from https://github.com/crosswalk-project/tizen-extensions-crosswalk
+
+if [ ! `which cpplint.py` ]; then
+   echo -en "\nPlease make sure cpplint.py is in your PATH. "
+   echo -e "It is part of depot_tools inside Chromium repository."
+   exit 1
+fi
+
+# Store current dir and change to repository root dir.
+OLD_PWD=$PWD
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SELF_DIR/..
+
+# TODO(vignatti): maybe we should remove the OZONE_ prefix from all header
+# guards and activate
+FILTERS="-build/header_guard"
+
+cpplint.py --filter="$FILTERS" $(find \
+                               \( -name '*.h' -o -name '*.cc' \) )
+
+# Return to previous dir and return the code returned by cpplint.py
+RET_VAL=$?
+cd $OLD_PWD
+exit $RET_VAL
