0a9bc0cbd2063a16c8f8b88922d4c822f2829cda
Kondapally Kalyan <kalyan.kondapally@intel.com>
2014-02-27 10:49:54 +0200

    Gardening: Fix linking issues.
    We need to implement few free functions now. Atleast static_component
    build should work fine now but I am sure that this breaks shared component
    build. However, I dont want to keep this update pending on that.
    I will try to fix these things upstream.
    
    Effecting CLS:
    https://codereview.chromium.org/174223004
    https://codereview.chromium.org/175573002
    https://codereview.chromium.org/176893002/
    
    (cherry picked from commit 8f343a6554891e513c7cd5edbc47a69f1749f169)
    
    Conflicts:
    	impl/desktop_aura/impl_view.gypi

---
+++ impl/ozone_util.cc
@@ -0,0 +1,41 @@
+// Copyright 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ozone/impl/ozone_util.h"
+
+#include "chrome/browser/ui/views/tabs/window_finder.h"
+#include "ozone/impl/desktop_window_tree_host_wayland.h"
+
+namespace ui {
+
+bool GetCustomFramePrefDefault() {
+  return true;
+}
+
+}  // namespace ui
+
+bool IsFullScreenMode() {
+  std::vector<aura::Window*> all_windows =
+      ozonewayland::DesktopWindowTreeHostWayland::GetAllOpenWindows();
+  // Only the topmost window is checked. This works fine in the most cases, but
+  // it may return false when there are multiple displays and one display has
+  // a fullscreen window but others don't. See: crbug.com/345484
+  if (all_windows.empty())
+    return false;
+
+  views::Widget* widget =
+      views::Widget::GetWidgetForNativeWindow(all_windows[0]);
+  return widget && widget->IsFullscreen();
+}
+
+// TODO(Kalyan): Remove this once https://codereview.chromium.org/176893002/
+// is fixed.
+aura::Window* GetLocalProcessWindowAtPoint(
+    chrome::HostDesktopType host_desktop_type,
+    const gfx::Point& screen_point,
+    const std::set<aura::Window*>& ignore) {
+  return NULL;
+}
+
+
