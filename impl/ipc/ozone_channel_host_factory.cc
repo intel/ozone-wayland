e954be08411fb113b7655ab5346716fe5c44e849
Kondapally Kalyan <kalyan.kondapally@intel.com>
2014-03-02 08:35:03 +0200

    IPC: Introduce OzoneChannelHostFactory.
    Currently, OzoneDisplay is responsible for establishing the Channel
    connection between ChannelHost and Channel. This should be transparent
    in OzoneDisplay and should be handled in content. This patch introduces
    OzoneChannelHostFactory which will be responsible for either establishing
    IPC connection with DisplayChannel or just creating InProcessEventConverter,
    depending on inprocess GPU usage. The idea here is that, we should be able
    to establish the Browser process side connection in BrowserMainLoop as we
    do in case of BrowserGpuChannelHostFactory.

---
+++ impl/ipc/ozone_channel_host_factory.cc
@@ -0,0 +1,49 @@
+// Copyright 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ozone/impl/ipc/ozone_channel_host_factory.h"
+
+#include "ozone/impl/ipc/display_channel_host.h"
+#include "ozone/ui/events/event_converter_in_process.h"
+#include "ozone/ui/events/event_factory_ozone_wayland.h"
+
+namespace content {
+OzoneChannelHostFactory* OzoneChannelHostFactory::instance_ = NULL;
+
+void OzoneChannelHostFactory::Initialize(bool establish_gpu_channel) {
+  DCHECK(!instance_);
+  instance_ = new OzoneChannelHostFactory(establish_gpu_channel);
+}
+
+void OzoneChannelHostFactory::Terminate() {
+  DCHECK(instance_);
+  delete instance_;
+  instance_ = NULL;
+}
+
+OzoneChannelHostFactory* OzoneChannelHostFactory::instance() {
+  return instance_;
+}
+
+OzoneChannelHostFactory::OzoneChannelHostFactory(bool establish_gpu_channel)
+    : event_converter_(NULL) {
+  event_converter_ = new ui::EventConverterInProcess();
+  ui::EventFactoryOzoneWayland* event_factory =
+    ui::EventFactoryOzoneWayland::GetInstance();
+  event_factory->SetEventConverterOzoneWayland(event_converter_);
+  event_converter_->SetWindowChangeObserver(
+      event_factory->GetWindowChangeObserver());
+  event_converter_->SetOutputChangeObserver(
+      event_factory->GetOutputChangeObserver());
+  if (establish_gpu_channel)
+    host_ = new OzoneDisplayChannelHost();
+}
+
+OzoneChannelHostFactory::~OzoneChannelHostFactory() {
+  ui::EventFactoryOzoneWayland::GetInstance()->
+      SetEventConverterOzoneWayland(NULL);
+  delete event_converter_;
+}
+
+}  // namespace content
---
+++ impl/ipc/ozone_channel_host_factory.h
@@ -0,0 +1,36 @@
+// Copyright 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef OZONE_IMPL_IPC_OZONE_CHANNEL_HOST_FACTORY_H_
+#define OZONE_IMPL_IPC_OZONE_CHANNEL_HOST_FACTORY_H_
+
+#include "ozone/impl/ipc/display_channel_host.h"
+
+namespace ui {
+class EventConverterInProcess;
+}
+
+namespace content {
+class OzoneDisplayChannelHost;
+// This class is responsible for establishing gpu channel connection between
+// OzoneDisplayChannel and ChannelHost.
+class OzoneChannelHostFactory {
+ public:
+  static void Initialize(bool establish_gpu_channel);
+  static void Terminate();
+  static OzoneChannelHostFactory* instance();
+
+  explicit OzoneChannelHostFactory(bool establish_gpu_channel);
+  virtual ~OzoneChannelHostFactory();
+
+ private:
+  ui::EventConverterInProcess* event_converter_;
+  OzoneDisplayChannelHost* host_;
+  static OzoneChannelHostFactory* instance_;
+  DISALLOW_COPY_AND_ASSIGN(OzoneChannelHostFactory);
+};
+
+}  // namespace content
+
+#endif  // OZONE_IMPL_IPC_OZONE_CHANNEL_HOST_FACTORY_H_
