954da1e39836d620f785c45ab55b4f517c17fc1f
Tiago Vignatti <tiago.vignatti@intel.com>
2014-05-07 20:50:54 -0300

    platform: Move all platform definitions to anonymous namespace
    By https://codereview.chromium.org/247963002:
    
    "There is no reason to expose the platform objects. They are constructed via a
    static function and never used except via the OzonePlatform base class."

--- platform/ozone_platform_wayland.cc
+++ platform/ozone_platform_wayland.cc
@@ -4,28 +4,58 @@
 
 #include "ozone/platform/ozone_platform_wayland.h"
 
+#include "ozone/ui/cursor/cursor_factory_ozone_wayland.h"
+#if defined(TOOLKIT_VIEWS) && !defined(OS_CHROMEOS)
+#include "ozone/ui/desktop_aura/desktop_factory_wayland.h"
+#endif
+#include "ozone/ui/events/event_factory_ozone_wayland.h"
+#include "ozone/ui/gfx/surface_factory_wayland.h"
+#include "ozone/ui/ime/input_method_context_factory_wayland.h"
+#include "ozone/wayland/ozone_hardware_display.h"
+#include "ui/base/cursor/ozone/cursor_factory_ozone.h"
+#include "ui/ozone/ozone_platform.h"
+
 namespace ui {
 
-OzonePlatformWayland::OzonePlatformWayland() {}
+namespace {
 
-OzonePlatformWayland::~OzonePlatformWayland() {}
+// OzonePlatform for Wayland
+//
+// This platform is Linux with the Wayland display server.
+class OzonePlatformWayland : public OzonePlatform {
+ public:
+  OzonePlatformWayland() {}
 
-gfx::SurfaceFactoryOzone* OzonePlatformWayland::GetSurfaceFactoryOzone() {
-  return &surface_factory_ozone_;
-}
+  virtual ~OzonePlatformWayland() {}
 
-ui::EventFactoryOzone* OzonePlatformWayland::GetEventFactoryOzone() {
-  return &event_factory_ozone_;
-}
+  // OzonePlatform:
+  virtual gfx::SurfaceFactoryOzone* GetSurfaceFactoryOzone() OVERRIDE {
+    return &surface_factory_ozone_;
+  }
+  virtual ui::EventFactoryOzone* GetEventFactoryOzone() OVERRIDE {
+    return &event_factory_ozone_;
+  }
+  virtual ui::InputMethodContextFactoryOzone*
+  GetInputMethodContextFactoryOzone() OVERRIDE {
+    return &input_method_context_factory_;
+  }
+  virtual ui::CursorFactoryOzone* GetCursorFactoryOzone() OVERRIDE {
+    return &cursor_factory_ozone_;
+  }
 
-ui::InputMethodContextFactoryOzone*
-OzonePlatformWayland::GetInputMethodContextFactoryOzone() {
-  return &input_method_context_factory_;
-}
+ private:
+  ozonewayland::OzoneHardwareDisplay hardware_display_;
+  gfx::SurfaceFactoryWayland surface_factory_ozone_;
+  ui::EventFactoryOzoneWayland event_factory_ozone_;
+  ui::InputMethodContextFactoryWayland input_method_context_factory_;
+  ui::CursorFactoryOzoneWayland cursor_factory_ozone_;
+#if defined(TOOLKIT_VIEWS) && !defined(OS_CHROMEOS)
+  views::DesktopFactoryWayland desktop_factory_ozone_;
+#endif
+  DISALLOW_COPY_AND_ASSIGN(OzonePlatformWayland);
+};
 
-ui::CursorFactoryOzone* OzonePlatformWayland::GetCursorFactoryOzone() {
-  return &cursor_factory_ozone_;
-}
+}  // namespace
 
 OzonePlatform* CreateOzonePlatformWayland() { return new OzonePlatformWayland; }
 
--- platform/ozone_platform_wayland.h
+++ platform/ozone_platform_wayland.h
@@ -5,48 +5,12 @@
 #ifndef OZONE_PLATFORM_OZONE_PLATFORM_WAYLAND_H_
 #define OZONE_PLATFORM_OZONE_PLATFORM_WAYLAND_H_
 
-#include "ozone/ui/cursor/cursor_factory_ozone_wayland.h"
-#if defined(TOOLKIT_VIEWS) && !defined(OS_CHROMEOS)
-#include "ozone/ui/desktop_aura/desktop_factory_wayland.h"
-#endif
-#include "ozone/ui/events/event_factory_ozone_wayland.h"
-#include "ozone/ui/gfx/surface_factory_wayland.h"
-#include "ozone/ui/ime/input_method_context_factory_wayland.h"
-#include "ozone/wayland/ozone_hardware_display.h"
-#include "ui/base/cursor/ozone/cursor_factory_ozone.h"
-#include "ui/ozone/ozone_platform.h"
-
 namespace ui {
 
-// OzonePlatform for Wayland
-//
-// This platform is Linux with the wayland display server.
-class OzonePlatformWayland : public OzonePlatform {
- public:
-  OzonePlatformWayland();
-  virtual ~OzonePlatformWayland();
-
-  virtual gfx::SurfaceFactoryOzone* GetSurfaceFactoryOzone() OVERRIDE;
-  virtual ui::EventFactoryOzone* GetEventFactoryOzone() OVERRIDE;
-  virtual ui::InputMethodContextFactoryOzone*
-      GetInputMethodContextFactoryOzone() OVERRIDE;
-  virtual ui::CursorFactoryOzone* GetCursorFactoryOzone() OVERRIDE;
-
- private:
-  ozonewayland::OzoneHardwareDisplay hardware_display_;
-  gfx::SurfaceFactoryWayland surface_factory_ozone_;
-  ui::EventFactoryOzoneWayland event_factory_ozone_;
-  ui::InputMethodContextFactoryWayland input_method_context_factory_;
-  ui::CursorFactoryOzoneWayland cursor_factory_ozone_;
-#if defined(TOOLKIT_VIEWS) && !defined(OS_CHROMEOS)
-  views::DesktopFactoryWayland desktop_factory_ozone_;
-#endif
-
-  DISALLOW_COPY_AND_ASSIGN(OzonePlatformWayland);
-};
+class OzonePlatform;
 
 // Constructor hook for use in ozone_platform_list.cc
-OZONE_WAYLAND_EXPORT OzonePlatform* CreateOzonePlatformWayland();
+OzonePlatform* CreateOzonePlatformWayland();
 
 }  // namespace ui
 
