ff667a7e3815f7aac187cf1d0d7cf2eb838c94ec
Kondapally Kalyan <kalyan.kondapally@intel.com>
2014-05-15 19:46:39 +0300

    Update Scripts for Spring release.

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -2,17 +2,29 @@
 
 PATCH_DIR=`pwd`/src/ozone/patches/
 HACKING_BRANCH=master-ozone
+HACKING_OZONE_BRANCH=master-ozonewayland
+RELEASE_BRANCH=branch_1916
+RELEASE_OZONE_BRANCH=Milestone-Spring
 
 echo "Ozone-Wayland: patching Chromium"
 cd src/
 
 # we switch to $HACKING_BRANCH before anything
-git checkout master
+git checkout $RELEASE_BRANCH
 
 exists=`git show-ref refs/heads/$HACKING_BRANCH`
 if [ -n "$exists" ]; then
   git branch -D $HACKING_BRANCH
 fi
 
-git checkout -b $HACKING_BRANCH master
+git checkout -b $HACKING_BRANCH $RELEASE_BRANCH
+
+cd ozone/
+git checkout $RELEASE_OZONE_BRANCH
+exists=`git show-ref refs/heads/$HACKING_OZONE_BRANCH`
+if [ -n "$exists" ]; then
+  git branch -D $HACKING_OZONE_BRANCH
+fi
+git checkout -b $HACKING_OZONE_BRANCH $RELEASE_OZONE_BRANCH
+cd ..
 git am $PATCH_DIR/00*
