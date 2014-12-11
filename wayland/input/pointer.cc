a7c3369e57ed4c14e2ae958f85221cc8f1b4f640
Joone Hur <joone.hur@intel.com>
2014-12-11 15:50:41 -0800

    Do not uninitialize input devices when disconnecting the devices.
    Each instance of WaylandPointer/WaylandKeyboard is deleted when
    the device is disconnected so we don't need to call OnSeatCapabilities
    method in order to uninitialize the input device.
    
    Bug=#338

--- wayland/input/pointer.cc
+++ wayland/input/pointer.cc
@@ -46,9 +46,6 @@ void WaylandPointer::OnSeatCapabilities(wl_seat *seat, uint32_t caps) {
       cursor_->SetInputPointer(input_pointer_);
     wl_pointer_set_user_data(input_pointer_, this);
     wl_pointer_add_listener(input_pointer_, &kInputPointerListener, this);
-  } else if (!(caps & WL_SEAT_CAPABILITY_POINTER)
-                && cursor_->GetInputPointer()) {
-    cursor_->SetInputPointer(NULL);
   }
 }
 
