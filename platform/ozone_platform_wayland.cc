4c6f06671c5332498145d6bb2b55e7962e96b7f7
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-01-21 11:05:30 +0200

    Return a nullptr in OzonePlatformWayland::CreateNativeDisplayDelegate.
    We were returning a default delegate that had stubs in all methods, but
    was not an exported symbol. To fix the shared_library build, just return
    a nullptr instead.

--- platform/ozone_platform_wayland.cc
+++ platform/ozone_platform_wayland.cc
@@ -76,7 +76,7 @@ class OzonePlatformWayland : public OzonePlatform {
   }
 
   scoped_ptr<NativeDisplayDelegate> CreateNativeDisplayDelegate() override {
-    return scoped_ptr<NativeDisplayDelegate>(new NativeDisplayDelegateOzone());
+    return nullptr;
   }
 
   void InitializeUI() override {
