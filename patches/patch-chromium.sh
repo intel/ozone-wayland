3397d40726ecb1169a256434a5c95c9df9d5540b
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-01-27 23:32:06 +0200

    Update patch-chromium.sh for M41.

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -2,24 +2,36 @@
 
 PATCH_DIR=`pwd`/src/ozone/patches/
 HACKING_BRANCH=master-ozone
+HACKING_OZONE_BRANCH=master-ozonewayland
+RELEASE_BRANCH=branch_2272
+RELEASE_OZONE_BRANCH=Milestone-Carnival
 
 echo "Ozone-Wayland: patching Chromium"
 cd src/
 
 # we switch to $HACKING_BRANCH before anything
-git checkout master
+git checkout $RELEASE_BRANCH
 
+# Chromium patches.
 exists=`git show-ref refs/heads/$HACKING_BRANCH`
 if [ -n "$exists" ]; then
   git branch -D $HACKING_BRANCH
 fi
 
-git checkout -b $HACKING_BRANCH master
+git checkout -b $HACKING_BRANCH $RELEASE_BRANCH
 git am $PATCH_DIR/00*
+
+# Switch ozone-wayland branch.
+cd ozone/
+git checkout $RELEASE_OZONE_BRANCH
+exists=`git show-ref refs/heads/$HACKING_OZONE_BRANCH`
+if [ -n "$exists" ]; then
+  git branch -D $HACKING_OZONE_BRANCH
+fi
+git checkout -b $HACKING_OZONE_BRANCH $RELEASE_OZONE_BRANCH
+cd ..
 
 # jump now to WebRTC dir and apply the needed patches there
 cd third_party/webrtc/
 git reset --hard origin/master
 git am $PATCH_DIR/100*
-
-
