1c5fc544c0ee9f6f1695f75d3a1a61ff9edeface
Kalyan Kondapally <kalyan.kondapally@intel.com>
2015-04-14 23:50:30 -0700

    Update Patch script

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -1,20 +1,31 @@
+
 #!/bin/sh
 
 PATCH_DIR=`pwd`/src/ozone/patches/
 HACKING_BRANCH=master-ozone
+HACKING_OZONE_BRANCH=master-ozonewayland
+RELEASE_BRANCH=branch_2357
+RELEASE_OZONE_BRANCH=Milestone-Hood
 
 echo "Ozone-Wayland: patching Chromium"
 cd src/
 
 # we switch to $HACKING_BRANCH before anything
-git checkout master
+git checkout $RELEASE_BRANCH
 
 exists=`git show-ref refs/heads/$HACKING_BRANCH`
 if [ -n "$exists" ]; then
   git branch -D $HACKING_BRANCH
 fi
 
-git checkout -b $HACKING_BRANCH master
+git checkout -b $HACKING_BRANCH $RELEASE_BRANCH
+
+cd ozone/
+git checkout $RELEASE_OZONE_BRANCH
+exists=`git show-ref refs/heads/$HACKING_OZONE_BRANCH`
+if [ -n "$exists" ]; then
+  git branch -D $HACKING_OZONE_BRANCH
+fi
+git checkout -b $HACKING_OZONE_BRANCH $RELEASE_OZONE_BRANCH
+cd ..
 git am $PATCH_DIR/00*
-
-
