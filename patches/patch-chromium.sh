b8e0d80199c963bfbf68815fd5453cd0d0a73fb3
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2015-08-20 12:09:51 +0200

    patch-chromium.sh: Adjust to Milestone-Trask.

--- patches/patch-chromium.sh
+++ patches/patch-chromium.sh
@@ -2,19 +2,18 @@
 
 PATCH_DIR=`pwd`/src/ozone/patches/
 HACKING_BRANCH=master-ozone
+RELEASE_BRANCH=45.0.2454.37
 
 echo "Ozone-Wayland: patching Chromium"
 cd src/
 
 # we switch to $HACKING_BRANCH before anything
-git checkout master
+git checkout $RELEASE_BRANCH
 
 exists=`git show-ref refs/heads/$HACKING_BRANCH`
 if [ -n "$exists" ]; then
   git branch -D $HACKING_BRANCH
 fi
 
-git checkout -b $HACKING_BRANCH master
+git checkout -b $HACKING_BRANCH $RELEASE_BRANCH
 git am $PATCH_DIR/00*
-
-
