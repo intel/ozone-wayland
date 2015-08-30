1f877a7e804127b592d0bf8f805db524fc767473
Kalyan Kondapally <kalyan.kondapally@intel.com>
2015-08-30 15:47:42 -0700

    Fix header include

--- platform/client_native_pixmap_factory_wayland.cc
+++ platform/client_native_pixmap_factory_wayland.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/ozone/platform/drm/common/client_native_pixmap_factory_drm.h"
+#include "ozone/platform/client_native_pixmap_factory_wayland.h"
 
 #include "ui/ozone/common/stub_client_native_pixmap_factory.h"
 
