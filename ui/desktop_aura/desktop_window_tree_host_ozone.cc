24613e4f15a313efe9b53658feba1928442c3794
Kalyan Kondapally <kalyan.kondapally@intel.com>
2015-09-11 18:57:15 -0700

    Gardening: Adapt to https://codereview.chromium.org/1302233006

--- ui/desktop_aura/desktop_window_tree_host_ozone.cc
+++ ui/desktop_aura/desktop_window_tree_host_ozone.cc
@@ -944,14 +944,14 @@ gfx::Size DesktopWindowTreeHostOzone::AdjustSize(
 
 gfx::Rect DesktopWindowTreeHostOzone::ToDIPRect(
     const gfx::Rect& rect_in_pixels) const {
-  gfx::RectF rect_in_dip = rect_in_pixels;
+  gfx::RectF rect_in_dip = gfx::RectF(rect_in_pixels);
   GetRootTransform().TransformRectReverse(&rect_in_dip);
   return gfx::ToEnclosingRect(rect_in_dip);
 }
 
 gfx::Rect DesktopWindowTreeHostOzone::ToPixelRect(
     const gfx::Rect& rect_in_dip) const {
-  gfx::RectF rect_in_pixels = rect_in_dip;
+  gfx::RectF rect_in_pixels = gfx::RectF(rect_in_dip);
   GetRootTransform().TransformRect(&rect_in_pixels);
   return gfx::ToEnclosingRect(rect_in_pixels);
 }
